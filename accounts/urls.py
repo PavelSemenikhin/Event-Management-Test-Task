e06b50def45b04502fee79618c4296cf6c597d23
Pavlo Semenikhin <pashock1337@gmail.com>
2025-11-24 22:24:46 +0200

    Add JWT endpoints and register endpoints

---
+++ accounts/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+from accounts.views import RegisterViewSet
+
+
+app_name = "accounts"
+router = DefaultRouter()
+router.register("register", RegisterViewSet, basename="register")
+
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+]
