a9d3af7cfa3399817fa9e96d96c4f5a8d1224fb6
Pavlo Semenikhin <pashock1337@gmail.com>
2025-11-24 20:52:45 +0200

    Add user model with migration

---
+++ accounts/models.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    pass
+
+    class Meta:
+        ordering = ["-date_joined"]
+
+    def __str__(self):
+        return self.username
