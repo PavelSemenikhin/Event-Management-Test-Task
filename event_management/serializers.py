22a5211f470c951e16e6e3800c2a009ba16552b0
Pavlo Semenikhin <pashock1337@gmail.com>
2025-11-24 22:26:27 +0200

    Add Event Serializers

---
+++ event_management/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+
+from event_management.models import Event, EventRegistration
+
+
+class EventCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Event
+        fields = ("id", "title", "description", "date", "location", "organizer")
+        read_only_fields = ("id",)
+
+
+class EventReadSerializer(serializers.ModelSerializer):
+    organizer = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field="username",
+    )
+
+    class Meta:
+        model = Event
+        fields = ("id", "title", "description", "date", "location", "organizer")
+        read_only_fields = ("id",)
+
+
+class EventRegistrationSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = EventRegistration
+        fields = ("id", "created_at")
+        read_only_fields = ("id", "created_at")
